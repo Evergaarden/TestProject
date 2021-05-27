df00912af02ab8d133ae42eafac767693e78b5a9
Evergaarden <sazuki.y@gmail.com user=evergaarden>
2021-05-27 23:32:48 +0300

    test commit

---
+++ ola.js
@@ -0,0 +1,54 @@
+// 1 переменные
+// var, const, let
+// const firstName = 'Bogdan'
+// // const lastName = 'Dupak' // string
+// const isProgrammer = true // boolean
+// // const age = 24 // number
+//
+// const t = 'private'
+// const y = 'some value'
+//
+// // const if = 'mkef' // error
+// // const 54545withNUm = 5 // error
+//
+// // 2. мутирование
+// // console.log('Имя человека: ' + firstName + ', а возраст человека: ' + age)
+// // alert ('Имя человека: ' + firstName + ', а возраст человека: ' + age)
+// // const lastName = prompt(message: 'Введите фамилию')
+// // alert(firstName + ' ' + lastName)
+//
+// // 3. операторы
+// let currentYear = 2021
+// const birthYear = 1996
+// // const age = currentYear - birthYear
+// const a = 10
+// const b = 5
+// let c = 32
+// c = c + a
+// c += a
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(currentYear++)
+// console.log(c)
+
+// 4. типы данных
+// const isProgrammer = true
+// const name = 'Bogdan'
+// const age = 24
+// let x
+//
+// console.log(typeof isProgrammer)
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof x
+// console.log(null)
+
+// 5. приоритет операторов
+const fullAge = 24
+const birthYear = 1996
+const currentYear = 2021
+
+// > < >= <=
+const isFullAge = (currentYear - birthYear) >= fullAge // 
